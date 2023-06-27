c46f07be6f525206efe791c97612b8fbf4e3c1c2
bruuno <bruuno.dsa@gmail.com>
2023-06-27 17:43:25 -0300

    Estrutura visual

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { Trash } from 'phosphor-react';
+import { ThumbsUp } from 'phosphor-react';
+import styles from "./Comment.module.css";
+
+
+export function Comment() {
+  return (
+    <div className={styles.Comment}>
+      <img src="https://github.com/dsabruno.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Bruno Alves</strong>
+              <time title='27 de Junho às 08:30h' dateTime='2023-06-27 08:36:45'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏 </p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
